7f8cc9d465dcb18e6c9ced49ad5e9d3d470c8304
bobby <bobbyisbest3@gmail.com>
2019-02-06 21:17:00 -0500

    Simplifying how color works

--- cmsc630/Image.py
+++ cmsc630/Image.py
@@ -6,31 +6,40 @@ class Image:
     COLOR_RED = 0
     COLOR_GREEN = 1
     COLOR_BLUE = 2
-    COLOR_RGB = 3
+    COLOR_GRAYSCALE = 3
 
     def __init__(self, matrix):
         self.matrix = matrix
+        self.matrix_gray = None
+        self.histogram = None
+        self.histogram_gray = None
 
-    def getHistogram(self, color=COLOR_RGB):
-        hist = np.zeros((255,3))
-        for row in self.matrix:
-            for column in row:
-                hist[column[0]][0] += 1
-                hist[column[1]][1] += 1
-                hist[column[2]][2] += 1
-        return hist
+    def getHistogram(self, color=None):
+        if color == self.COLOR_GRAYSCALE:
+            if self.histogram_gray is None:
+                hist = np.zeros((255))
+                for row in self.getGrayscale():
+                    for column in row:
+                        hist[column] += 1
+                self.histogram_gray = hist
+            return self.histogram_gray
+        else:
+            if self.histogram is None:
+                hist = np.zeros((255,3))
+                for row in self.getMatrix():
+                    for column in row:
+                        hist[column[0]][0] += 1
+                        hist[column[1]][1] += 1
+                        hist[column[2]][2] += 1
+                self.histogram = hist
+            return self.histogram
     
-    def asMatrix(self):
+    def getMatrix(self, color=None):
+        if color == self.COLOR_GRAYSCALE:
+            return self.getGrayscale()
+        if color is not None:
+            return self.matrix[:,:,color]
         return self.matrix
-    
-    def getRed(self):
-        return self.matrix[:,:,self.COLOR_RED]
-    
-    def getGreen(self):
-        return self.matrix[:,:,self.COLOR_GREEN]
-    
-    def getBlue(self):
-        return self.matrix[:,:,self.COLOR_BLUE]
     
     def getGrayscale(self, luminosity=False):
         """Returns a grayscale version of the RGB image by
@@ -46,12 +55,14 @@ class Image:
             ndarray: A single channel matrix of shape (height, width)
                 representing the pixel intensities
         """
-        if luminosity:
-            w = [0.30, 0.59, 0.11]
-        else:
-            w = [0.33, 0.33, 0.33]
+        if self.matrix_gray is None:
+            if luminosity:
+                w = [0.30, 0.59, 0.11]
+            else:
+                w = [0.33, 0.33, 0.33]
+            self.matrix_gray = np.sum(np.multiply(self.matrix, w), axis=2)
 
-        return np.sum(np.multiply(self.matrix, w), axis=2)
+        return self.matrix_gray
 
 
 def loadImages(path):
