3dbe0214ba85911695ac9e175ef59dfd21bc3a10
bobby <bobbyisbest3@gmail.com>
2019-02-09 18:20:57 -0500

    Implementing uniform quantization

--- cmsc630/Image.py
+++ cmsc630/Image.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import copy
 import numpy as np
 
 class Image:
@@ -12,28 +13,33 @@ class Image:
     COLOR_BLUE = 2
     COLOR_RGB = 3
     COLOR_GRAYSCALE = 4
+
+    # QUANTIZATION TYPES
+    QUANT_UNIFORM = "uniform"
+    QUANT_MEDIAN = "median"
+    QUANT_MEAN = "mean"
     
     # CLASS FILTER CONSTANTS
     FILTER_LINEAR = "linear"
     FILTER_MEDIAN = "median"
 
     def __init__(self, matrix):
-        self.matrix = {
-            self.COLOR_RED:         matrix[:,:,self.COLOR_RED],
-            self.COLOR_GREEN:       matrix[:,:,self.COLOR_GREEN],
-            self.COLOR_BLUE:        matrix[:,:,self.COLOR_BLUE],
-            self.COLOR_RGB:         matrix,
-            self.COLOR_GRAYSCALE:   None
-        }
-        self.histogram = {
-            self.COLOR_RED:         None,
-            self.COLOR_GREEN:       None,
-            self.COLOR_BLUE:        None,
-            self.COLOR_RGB:         None,
-            self.COLOR_GRAYSCALE:   None
-        }
+        self.matrix = [
+            matrix[:,:,self.COLOR_RED],
+            matrix[:,:,self.COLOR_GREEN],
+            matrix[:,:,self.COLOR_BLUE],
+            matrix,
+            None
+        ]
+        self.histogram = [None, None, None, None, None]
+    
+    def copy(self):
+        ret = Image(self.getMatrix())
+        ret.matrix = copy.deepcopy(self.matrix)
+        ret.histogram = copy.deepcopy(self.histogram)
+        return ret
 
-    def getHistogram(self, color=self.COLOR_RGB):
+    def getHistogram(self, color=3):
         """Returns a histogram of the Image's pixel values.
 
         Arguments:
@@ -52,14 +58,14 @@ class Image:
                     self.getHistogram(self.COLOR_BLUE),
                 ), axis=2)
             else: # The single-channel histograms (R, G, B, Gray) can be calculated as follows
-                self.histogram[color] = np.zeros((255,1))
+                self.histogram[color] = np.zeros((255,1), dtype="int")
                 for row in self.getMatrix(color):
                     for pixel in row:
                         self.histogram[color][pixel] += 1
         
         return self.histogram[color]
 
-    def getMatrix(self, color=None):
+    def getMatrix(self, color=3):
         """Returns the Image's pixel matrix with
         the requested color channels
 
@@ -75,9 +81,9 @@ class Image:
                 return self.getGrayscale()
             if color == self.COLOR_RGB:
                 self.matrix[color] = np.stack((
-                    self.getHistogram(self.COLOR_RED),
-                    self.getHistogram(self.COLOR_GREEN),
-                    self.getHistogram(self.COLOR_BLUE),
+                    self.getMatrix(self.COLOR_RED),
+                    self.getMatrix(self.COLOR_GREEN),
+                    self.getMatrix(self.COLOR_BLUE),
                 ), axis=2)
         return self.matrix[color]
 
@@ -106,23 +112,23 @@ class Image:
                 w = [0.30, 0.59, 0.11]
             else:
                 w = [0.33, 0.33, 0.33]
-            self.matrix[self.COLOR_GRAYSCALE] = np.sum(np.multiply(self.matrix[self.COLOR_RGB], w), axis=2)
+            self.matrix[self.COLOR_GRAYSCALE] = np.sum(np.multiply(self.matrix[self.COLOR_RGB], w), axis=2, dtype="int")
 
         return self.matrix[self.COLOR_GRAYSCALE]
     
-    def makeSaltnPepperNoise():
+    def makeSaltnPepperNoise(self):
         """
         """
         # Add salt & pepper noise to R, G, and B
         return
     
-    def makeGaussianNoise():
+    def makeGaussianNoise(self):
         """
         """
         # Add gaussian noise to R, G, and B
         return
     
-    def equalize(color=Image.COLOR_RED):
+    def equalize(self, color=3):
         """
 
         Returns:
@@ -130,10 +136,67 @@ class Image:
         """
         return
     
-    def quantize():
-        return
+    def quantize(self, delta=16, technique='uniform', color=3):
+        """Returns a new copy of this Image object that has been quantized
+        on the desired channel
+
+        Arguments:
+            delta (int): The quantization level, i.e. max number of color values
+                that will be present in the resulting image
+            technique: Desired quantization technique, see class quantization constants
+            color (int): Desired color channel(s), see class color constants
+        
+        Returns:
+            Image: A new copy of the original image, quantized according to parameters
+        """
+        if(self.matrix[color] is None):
+            self.getMatrix(color)
+
+        ret = self.copy()
+
+        if color == self.COLOR_RGB:
+            ret.matrix[self.COLOR_RED] = self._quantize(ret, delta, technique, color=self.COLOR_RED)
+            ret.matrix[self.COLOR_GREEN] = self._quantize(ret, delta, technique, color=self.COLOR_GREEN)
+            ret.matrix[self.COLOR_BLUE] = self._quantize(ret, delta, technique, color=self.COLOR_BLUE)
+            ret.matrix[self.COLOR_RGB] = np.stack((
+                    ret.getMatrix(self.COLOR_RED),
+                    ret.getMatrix(self.COLOR_GREEN),
+                    ret.getMatrix(self.COLOR_BLUE),
+                ), axis=2)
+        else:
+            ret.matrix[color] = self._quantize(ret, delta, technique, color=color)
+
+        return ret
+    def _quantize(self, B, delta, technique, color):
+        """Helper function for `Image.quantize()`, performs the math to quantize a
+        single color channel
+
+        Arguments:
+            B (Image): The new copy Image produced in `Image.quantize()`
+            delta (int): The quantization level, i.e. max number of color values
+                that will be present in the resulting image
+            technique: Desired quantization technique, see class quantization constants
+            color (int): Desired color channel(s), see class color constants
+        
+        Returns:
+            ndarray: The color channel of B that has been quantized
+        """
+        # max_peak = np.max(ret.getHistogram(color))
+        # levels = list(range(0, max_peak+1, int(max_peak/delta)))
+        # if levels[-1] != max_peak: levels[-1] = max_peak
+        # print(len(levels), levels, max_peak)
+
+        buckets = list(range(0, 256, int(255/delta)))
+        if buckets[-1] != 255: buckets[-1] = 255
+        for i, _ in enumerate(buckets):
+            if i == 0: continue
+
+            bucket = list(range(buckets[i-1], buckets[i]))
+            B.matrix[color][np.where(np.isin(self.matrix[color][:], bucket))] = delta*i
+        return B.matrix[color]
+
     
-    def applyFilter(filter=None):
+    def applyFilter(self, filter=None):
         """
         """
         if filter is None:
--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 from cmsc630 import Image
+import numpy as np
 import matplotlib.pyplot as plt
 
-x = Image.fromDir("./train")
-print(x[0].shape)
+x = Image.fromDir("./train/cyl01.BMP")[0]
+y = x.quantize(color=Image.COLOR_GRAYSCALE)
+
+print(x.getMatrix(color=Image.COLOR_GRAYSCALE))
+print(y.matrix[Image.COLOR_GRAYSCALE])
+
+# plt.imshow(x.getGrayscale(), cmap='gray')
+# plt.imshow(y.getGrayscale(), cmap='gray')
+# plt.show()
+
+# x = np.array([5, 2, 6, 3])
+
+# print(x[np.where(x == 6)])
+# x[x == 6] = 5
+# print(x)
+
+# a = [np.array([
+#     [11, 12, 20, 14, 15],
+#     [16, 17, 18, 19, 20]
+# ])]
+# i = np.where(np.isin(a[0][:], [11, 20]))
+# print(i)
+# a[0][i] = 5
+# print(a[0])
